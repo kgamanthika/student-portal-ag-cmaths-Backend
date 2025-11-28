6919ce25089f32ca5c75ee3dd56b813aa421b14b
amesh <amesh@codevus.com>
2025-11-28 23:55:54 +0530

    finish backend

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config(); // load .env
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/studentPortal")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err));
+// Connect MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.log("❌ MongoDB Connection Error:", err));
 
 // Routes
 app.get("/", (req, res) => {
   res.send("Backend running...");
 });
-
-app.listen(5000, () => console.log("Server running on port 5000"));
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
@@ -29,3 +30,7 @@ app.use("/marks", marksRoutes);
 
 const sysOwnerRouter = require("./routes/systemOwner");
 app.use("/sysOwner", sysOwnerRouter);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
